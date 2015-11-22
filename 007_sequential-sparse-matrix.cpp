da456d89de2e6e4c47f71b8db7a624b9e541657c
Jian Xiao <jian.turing.xiao@outlook.com>
2015-11-22 17:46:13 +0800

    Fix some bugs, many bugs remaining

--- 007_sequential-sparse-matrix.cpp
+++ 007_sequential-sparse-matrix.cpp
@@ -8,7 +8,7 @@ using std::cout;
 template< typename T >
 SSparseMatrix< T >::SSparseMatrix( const SSparseMatrix& src_matrix ){
 	//Copy all the private member
-	_rows = src_matrix._row;
+	_rows = src_matrix._rows;
 	_cols = src_matrix._cols;
 	_elem_total = src_matrix._elem_total;
 	_capacity = src_matrix._capacity;
@@ -25,15 +25,19 @@ SSparseMatrix< T >::SSparseMatrix( const SSparseMatrix& src_matrix ){
 	memcpy( _begin_pos_of_row, src_matrix._begin_pos_of_row, _rows+2 );
 }
 
+/*
 //Assign operator
 template< typename T >
-SSparseMatrix< T >& SSparseMatrix::operator= ( const SSparseMatrix& src_matrix ){
+SSparseMatrix< T >& SSparseMatrix< T >::operator= ( const SSparseMatrix< T >& src_matrix ){
+	//HERE...
 	this->SSparseMatrix( src_matrix );
 }
 
+*/
+
 //Read Matrix from \istream is
 template< typename T >
-size_t SSparseMatrix< T >::read_content( istream& is ){
+int SSparseMatrix< T >::read_content( istream& is ){
 	value_type cur_e;
 	for( int i = 0; i < _rows; i++ ){
 		for( int j = 0; j < _cols; j ++ ){
@@ -48,10 +52,10 @@ size_t SSparseMatrix< T >::read_content( istream& is ){
 
 //Print row
 template< typename T >
-void SSparseMatrix< T >::print_row( size_t row )const{
+SSparseMatrix< T >& SSparseMatrix< T >::print_row( int row )const{
 	int cur_pos = _begin_pos_of_row[row];
-	for( size_t cur_col = 1; cur_col != _cols; cur_col ++ ){
-		if( _node[cur_pos+i].col = cur_col+1 ){
+	for( int j = 1; j <= _cols; j ++ ){
+		if( _node[cur_pos].col = j ){
 			cout << _node[cur_pos].elem << " ";
 			++cur_pos;
 		}else{
@@ -62,8 +66,8 @@ void SSparseMatrix< T >::print_row( size_t row )const{
 
 //Print Matrix
 template< typename T >
-void SSparseMatrix< T >::print()const{
-	for( size_t i = 1; i <= _rows; i++ ){
+SSparseMatrix< T >& SSparseMatrix< T >::print()const{
+	for( int i = 1; i <= _rows; i++ ){
 		print_row( i );
 		cout << endl;
 	}
@@ -72,10 +76,10 @@ void SSparseMatrix< T >::print()const{
 
 //Get elem
 template< typename T >
-T SSparseMatrix< T >::elem( size_t row_idx, size_t col_idx )const{
+T SSparseMatrix< T >::elem( int row_idx, int col_idx )const{
 	if( row_idx <= _rows && col_idx <= _cols ){
-		size_t cur_pos = _begin_pos_of_row[row_idx];
-		for( size_t cur_col = 1; cur_col <= _cols; cur_col ++ ){
+		int cur_pos = _begin_pos_of_row[row_idx];
+		for( int cur_col = 1; cur_col <= _cols; cur_col ++ ){
 			if( _node[cur_pos].col = col_idx ){
 				return _node[cur_pos].elem;
 			}
@@ -94,12 +98,13 @@ void SSparseMatrix< T >::clear(void){
 	delete []_elem_cout_in_row;
 	delete []_begin_pos_of_col;
 	_node = _elem_cout_in_col = _begin_pos_of_col = _elem_cout_in_row = _begin_pos_of_row = NULL;
+	_elem_total = 0;
 }
 
 //Multiply
 template< typename T >
 SSparseMatrix< T > operator* ( const SSparseMatrix< T >& matrix_a, const SSparseMatrix< T >& matrix_b ){
-	SSparseMatrix temp_matrix( matrix_a );
+	SSparseMatrix< T > temp_matrix( matrix_a );
 	temp_matrix.multiply( matrix_b );
 	return temp_matrix;
 }
@@ -107,23 +112,24 @@ SSparseMatrix< T > operator* ( const SSparseMatrix< T >& matrix_a, const SSparse
 //------------------------INTERNAL FUNCTION-----------------------
 
 template< typename T>
-void _generate_internal_data(void){
-	for( size_t cur_pos = 0; cur_pos != _elem_total; cur_pos++ ){
-		size_t j = _node[cur_pos].col;
-		size_t i = _node[cur_pos].row;
+void SSparseMatrix<T>::_generate_internal_data(void){
+	for( int cur_pos = 0; cur_pos != _elem_total; cur_pos++ ){
+		int j = _node[cur_pos].col;
+		int i = _node[cur_pos].row;
 		_elem_cout_in_col[j]++;
 		_elem_cout_in_row[i]++;
 	}
 
 	_begin_pos_of_col[1] = 0;
 	_begin_pos_of_row[1] = 0;
-	for( size_t j = 2; j <= _cols; j++ ){
-		_begin_pos_of_col[i] = _begin_pos_of_col[i-1] + _elem_cout_in_col[i-1];
+	for( int j = 2; j <= _cols; j++ ){
+		_begin_pos_of_col[j] = _begin_pos_of_col[j-1] + _elem_cout_in_col[j-1];
 	}
-	for( size_t i = 2; i <= _rows; i++ ){
+	for( int i = 2; i <= _rows+1; i++ ){ //为方便得到最后一行的终止位置，虚构第_row+1行
 		_begin_pos_of_row[i] = _begin_pos_of_row[i-1] + _elem_cout_in_row[i-1];
 	}
 }
+
 template< typename T >
 bool SSparseMatrix< T >::_equal( const SSparseMatrix<T>& matrix_b ){
 	if( _rows != matrix_b._rows 
@@ -132,7 +138,7 @@ bool SSparseMatrix< T >::_equal( const SSparseMatrix<T>& matrix_b ){
 		return false;
 	} else {
 		for( int i = 0; i != _elem_total; i++ ){
-			if( _node[i] != matrix_b._node[i] ){ return false; }
+			if( _node[i].elem != matrix_b._node[i].elem ){ return false; }
 		}
 	}
 	return true;
@@ -143,15 +149,15 @@ template< typename T >
 void SSparseMatrix< T >::_fast_transpose(){
 	if( !_elem_total ) return;
 	_generate_internal_data();
-	cur_pos_of_col = new int[_cols+1];
-	for( size_t j = 1; j <= _cols; j++ ){
+	int *cur_pos_of_col = new int[_cols+1];
+	for( int j = 1; j <= _cols; j++ ){
 		cur_pos_of_col[j] = _begin_pos_of_col[j];
 	}
 	//Begin transpose
 	SSparseMatrix temp_matrix( *this );	//Is this ok?
-	for( size_t src_pos = 0; src_pos != _elem_total; src_pos++ ){
-		size_t j = _node[src_pos].col;
-		size_t dest_pos = cur_pos_of_col[j];
+	for( int src_pos = 0; src_pos != _elem_total; src_pos++ ){
+		int j = _node[src_pos].col;
+		int dest_pos = cur_pos_of_col[j];
 		temp_matrix._node[dest_pos].row = _node[src_pos].col;
 		temp_matrix._node[dest_pos].col = _node[src_pos].row;
 		temp_matrix._node[dest_pos].elem = _node[src_pos].elem;
@@ -160,7 +166,7 @@ void SSparseMatrix< T >::_fast_transpose(){
 	*this = temp_matrix;
 }
 
-
+/*
 template< typename T >
 void SSparseMatrix< T >::_fast_multiply( const SSparseMatrix<T>& matrix_b ){
 	//Exclude the situation that the a._cols != b._rows
@@ -168,30 +174,30 @@ void SSparseMatrix< T >::_fast_multiply( const SSparseMatrix<T>& matrix_b ){
 	_generate_internal_data();
 	//Creat temp array to store the result & tempory matrix
 	T *temp_result = new T[matrix_b._cols+1];
-	for( size_t cur_col = 1; cur_col <= matrix_b._cols; cur_col++ ){
+	for( int cur_col = 1; cur_col <= matrix_b._cols; cur_col++ ){
 		temp_result[cur_col] = 0;
 	}
 	SSparseMatrix result_matrix( _rows, matrix_b._cols, _capacity );
 	//依次计算result-matrix的每一行(以metrix_a 的行)
-	for( size_t cur_row = 1; cur_row <= _rows; cur_row++ ){
-		size_t cur_elem_pos_of_a = _begin_pos_of_row[cur_row];
-		size_t cur_row_end_of_a  = _begin_pos_of_row[cur_row+1];
+	for( int cur_row = 1; cur_row <= _rows; cur_row++ ){
+		int cur_elem_pos_of_a = _begin_pos_of_row[cur_row];
+		int cur_row_end_of_a  = _begin_pos_of_row[cur_row+1];
 		//Deal with every elem in this row
 		while( cur_elem_pos_of_a++ < cur_row_end_of_a ){
 			//找到这个元素在b中对应的行号
 			col_of_cur_elem = _node[cur_elem_pos_of_a].col;
 			//确定b中行的起始位置和结束位置
-			size_t cur_elem_pos_of_b = matrix_b._begin_pos_of_row[col_of_a_elem];
-			size_t cur_row_end_of_b  = matrix_b._begin_pos_of_row[col_of_a_elem+1];
+			int cur_elem_pos_of_b = matrix_b._begin_pos_of_row[col_of_a_elem];
+			int cur_row_end_of_b  = matrix_b._begin_pos_of_row[col_of_a_elem+1];
 			//将此元素与b中对应行的每个元素相乘，并放入temp_result 中
 			while( cur_elem_pos_of_b++ < cur_row_end_of_b ){
-				temp_col = matrix_b._node[cur_elem_pos_of_b].col;
-				temp_result[temp_col] += _node[cur_elem_pos_of_a].elem
+				int j = matrix_b._node[cur_elem_pos_of_b].col;
+				temp_result[j] += _node[cur_elem_pos_of_a].elem
 							 * matrix_b._node[cur_elem_pos_of_b].elem;
 			}
 		}
 		//Compress and store
-		for( size_t cur_col = 1; cur_col <= matrix_b._cols; cur_col++ ){
+		for( int cur_col = 1; cur_col <= matrix_b._cols; cur_col++ ){
 			if( temp_result[cur_col] ){
 				//处理容量不足的情况
 				if( result_matrix._elem_total + 1 == result_matrix._capacity - 1 ){
@@ -204,19 +210,20 @@ void SSparseMatrix< T >::_fast_multiply( const SSparseMatrix<T>& matrix_b ){
 	*this = result_matrix;
 }
 
+*/
 
-//增加容量，一次1倍
+//Extend capacity, x2
 template< typename T >
-void SSparseMatrix< T >::_extend_capacity( size_t times ){
-	realloc( _node, _capacity * times );
+void SSparseMatrix< T >::_extend_capacity( int times ){
+	_node = (MatrixTriple<T>*)realloc( _node, _capacity * times );
 }
 
 
 //Append elem
 template< typename T >
-SSparseMatrix<T>& SSparseMatrix< T >::_push_back( size_t row, size_t col, T value ){
+SSparseMatrix<T>& SSparseMatrix< T >::_push_back( int row, int col, T value ){
 	++_elem_total;
-	if( _elem_total == _capacity - 1 ){  // _capacity-1 以确保有一个元素的余量
+	if( _elem_total == _capacity - 1 ){  // _capacity-1 以确保有一个元素的余量作为end
 		_extend_capacity();
 	}
 	_node[_elem_total-1].row = row; //_elem_total couts from 1, but position couts from 0
--- 007_sequential-sparse-matrix.h
+++ 007_sequential-sparse-matrix.h
@@ -16,28 +16,26 @@ template< typename T >
 class SSparseMatrix{
 public:
 	typedef T        value_type;
-	typedef unsigned size_type;
 
 public:
 	//When creat an object, MUST give its row and col
-	SSparseMatrix( size_t rows, size_t cols, size_t given_capacity = 0 )
+	SSparseMatrix( int rows, int cols, int given_capacity = 0 )
 		: _rows(rows), _cols(cols), _elem_total(0){
-		if( given_capacity == 0 ){
-			_capacity = 0.05 * _rows * _cols;
-		}
+		if( given_capacity == 0 ){ _capacity = 0.05 * _rows * _cols; }
+		else{ _capacity = given_capacity; }
 		_node = new MatrixTriple<T>[ _capacity ];
-		_elem_cout_in_col      = new int[_cols+1];
-		_begin_position_of_col = new int[_cols+1];
-		_elem_cout_in_row      = new int[_rows+1];
-		_begin_position_of_row = new int[_rows+2]; //为在矩阵相乘时方便得到最后一行的终止位置，虚构第_rows+1行
+		_elem_cout_in_col = new int[_cols+1];
+		_begin_pos_of_col = new int[_cols+1];
+		_elem_cout_in_row = new int[_rows+1];
+		_begin_pos_of_row = new int[_rows+2]; //为在矩阵相乘时方便得到最后一行的终止位置，虚构第_rows+1行
 		//Initilize all the array
-		for( size_t cur_row = 1; cur_row <= _rows; cur_row++ ){
-			_elem_cout_in_row[cur_row] = 0;
-			_begin_position_of_row[cur_row] = 0;
+		for( int i = 1; i <= _rows; i++ ){
+			_elem_cout_in_row[i] = 0;
+			_begin_pos_of_row[i] = 0;
 		}
-		for( size_t cur_col = 1; cur_col <= _cols; cur_col++ ){
-			_elem_cout_in_col[cur_col] = 0;
-			_begin_position_of_col[cur_col] = 0;
+		for( int j = 1; j <= _cols; j++ ){
+			_elem_cout_in_col[j] = 0;
+			_begin_pos_of_col[j] = 0;
 		}
 	}
 	//Copy constructor
@@ -46,58 +44,62 @@ public:
 	~SSparseMatrix(){
 		delete []_node;
 		delete []_elem_cout_in_col;
-		delete []_begin_position_of_col;
+		delete []_begin_pos_of_col;
 		delete []_elem_cout_in_row;
-		delete []_begin_position_of_row;
+		delete []_begin_pos_of_row;
 	}
+/*
 	//Assign operator
 	SSparseMatrix& operator= ( const SSparseMatrix& src_matrix );
 
+*/
 
 	//Public function 
-	size_t        read_content( void );
-	bool          empty()const { return !_elem_total; }
-	size_t        elem_total()const { return _elem_total; }
-	size_t        capacity()const { return _capacity; }
-	size_t        rows()const { return _rows; }
-	size_t        cols()const { return _cols; }
-	value_type    elem( size_t row_idx, size_t col_idx )const;
-	void          clear();
+	int     read_content( istream& is );
+	bool    empty()const { return !_elem_total; }
+	int     elem_total()const { return _elem_total; }
+	int     capacity()const { return _capacity; }
+	int     rows()const { return _rows; }
+	int     cols()const { return _cols; }
+	value_type   elem( int row_idx, int col_idx )const;
+	void         clear();
 	SSparseMatrix& transpose(){ _fast_transpose(); }
-	SSparseMatrix& print_row( size_t row )const;
+	SSparseMatrix& print_row( int row )const;
 	SSparseMatrix& print()const;
 	SSparseMatrix& multiply( const SSparseMatrix& matrix_b ){ _fast_multiply( matrix_b ); }
 	bool operator== ( const SSparseMatrix& matrix_b ){ return _equal( matrix_b ); }
 	bool operator!= ( const SSparseMatrix& matrix_b ){ return !(*this==matrix_b ); }
 	//friend template ` operator* ` 
-		//Is this usage correct?
-	friend SSparseMatrix< T >& operator* < T > ( const SSparseMatrix< T >& matrix_a, const SSparseMatrix< T >& matrix_b );
+		//Is this usage correct? --- maybe not, gcc says that `declaration of operator* as non-function`
+	template< class Type >
+	friend SSparseMatrix< Type >& operator* ( const SSparseMatrix< Type >& matrix_a, const SSparseMatrix< Type >& matrix_b );
+
 
 private:
 
-	size_t _rows; //coutn from 1
-	size_t _cols;
-	size_t _elem_total;
-	size_t _capacity;
+	int _rows; //coutn from 1
+	int _cols;
+	int _elem_total;
+	int _capacity; //cout from 1
 
 	MatrixTriple< T > *_node;	
 	//the array used for fast-transpose
 	//**Position begin from 0**
 	int *_elem_cout_in_col;
-	int *_begin_position_of_col;
+	int *_begin_pos_of_col;
 	//the array used for fast-multiply
 	int *_elem_cout_in_row;
-	int *_begin_position_of_row;
+	int *_begin_pos_of_row;
 
 private:
 	void _generate_internal_data();
 	bool _equal( const SSparseMatrix& matrix_b );
 	void _fast_transpose();
 	void _fast_multiply( const SSparseMatrix& matrix_b );
-	void _extend_capacity( size_t times = 2 );
-	void _push_back( size_t row, size_t col, T value );
+	void _extend_capacity( int times = 2 );
+	SSparseMatrix< T >& _push_back( int row, int col, T value );
 
 };
 
-#include"007_sequential-spare-matrix.cpp"
+#include"007_sequential-sparse-matrix.cpp"
 #endif
