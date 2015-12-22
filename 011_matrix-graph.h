9804a349339966803e9b8c8032ee1973bc55b224
Jian Xiao <jian.turing.xiao@outlook.com>
2015-12-22 14:32:11 +0800

    Fix copy constructor bug & change variable name
    fix the copy constructor bug that can't copy the correct content
     to avoid invalid pointer.
    change the \typename value_type to info_type

--- 011_matrix-graph.h
+++ 011_matrix-graph.h
@@ -2,7 +2,6 @@
 #define MATRIX_GRAPH_H_
 #include<iostream>
 #include<utility>
-#include<vector>
 #include<string>
 #include<cstring>
 #include<exception>
@@ -17,7 +16,7 @@ using namespace std;
 enum GraphKind_t { DiGraph, UnDiGraph, DiNetwork, UnDiNetwork };
 /**
  *  T1 : key_type
- *  T2 : value_type
+ *  T2 : info_type
  *  T3 : bool OR other_type to justify DiGraph/Network
  *       edge is two arcs
  */
@@ -25,38 +24,37 @@ template< typename T1, typename T2, typename T3, GraphKind_t K >
 class MGraph_basic{
 public:
 	typedef T1                     key_type;
-	typedef T2                     value_type;
+	typedef T2                     info_type;
 public:
 	MGraph_basic( const unsigned& given_vex_num, const unsigned& given_arc_num );
 	MGraph_basic( const MGraph_basic& given_graph );
 	~MGraph_basic( void );
-	void operator= ( const MGraph_basic& given_graph );
+	MGraph_basic& operator= ( const MGraph_basic& given_graph );
 
 	unsigned  arc_number()const{ return arc_num; }
 	unsigned  vex_number()const{ return vex_num; }
-	void      print_vex( ostream& os )const;
+	ostream  &print_vex( ostream& os )const;
 	ostream  &print_matrix( ostream& os )const;
 
 	istream  &read_content( istream& is );
-	void      add_vex( const T1& new_key, const T2& new_vex_value );
+	void      add_vex( const T1& new_key, const T2& new_vex_info );
 	void      remove_vex( const T1& old_vex );
 	void      add_arc( const T1& tail, const T1& head );
 	void      add_arc( const T1& tail, const T1& head, const T3& power );
 	void      remove_arc( const T1& tail, const T1& head );
 	unsigned  in_degree( const T1& key );
 	unsigned  out_degree( const T1& key );
-	T2       &get_value( const T1& key );
+	T2       &get_info( const T1& key );
 	unsigned  get_location( const T1& key );
-	void      edit_value( const T1& key, const T2& new_value );
+	void      edit_info( const T1& key, const T2& new_info );
 	void      edit_power( const T1& key1, const T1& key2, const T3& new_power );
-	void      adj_vex( const T1& key, vector< pair<T1, T2> >& vex_container );
+//	void      adj_vex( const T1& key, vector< pair<T1, T2> >& vex_container );
 	bool      is_adj( const T1& key_1, const T1& key_2 );
 
 private:
 	void init_matrix_with_zero( void );
 private:
 	T3 ** arc_matrix;
-//	vector<  pair<T1, T2>  > vexs; //map is a better choice, but it's too late when i know this.
 	pair< T1, T2 > * vexs;
 	unsigned vex_num;
 	unsigned arc_num;
@@ -94,8 +92,9 @@ MGraph_basic< T1, T2, T3, K >::MGraph_basic( const MGraph_basic& given_graph ) {
 	arc_matrix = new T3*[ vex_num ];
 	for( unsigned i = 0; i < vex_num; i++ ) {
 		arc_matrix[i] = new T3[ vex_num ];
+		memcpy( arc_matrix[i], given_graph.arc_matrix[i], vex_num );
 	}
-	memcpy( arc_matrix, given_graph.arc_matrix, vex_num * vex_num );
+	memcpy( arc_matrix, given_graph.arc_matrix, vex_num );
 	vexs = new pair<T1, T2>[ vex_num ];
 	memcpy( vexs, given_graph.vexs, sizeof( pair<T1, T2> ) * vex_num );
 }
@@ -128,7 +127,7 @@ void MGraph_basic< T1, T2, T3, K >::operator= ( const MGraph_basic& given_graph 
 template< typename T1, typename T2, typename T3, GraphKind_t K >
 void MGraph_basic< T1, T2, T3, K >::print_vex( ostream& os )const{
 	for( unsigned i = 0; i < vex_num; i++ ) {
-		os << vexs[i].first << vexs[i].second << endl;
+		os << vexs[i].first << ": " << vexs[i].second << endl;
 	}
 }
 
@@ -148,17 +147,17 @@ ostream& MGraph_basic< T1, T2, T3, K >::print_matrix( ostream& os )const{
 
 template< typename T1, typename T2, typename T3, GraphKind_t K >
 istream &MGraph_basic< T1, T2, T3, K >::read_content( istream& in ) {
-	if( !in ) { throw runtime_error( "istream invalid" ); }
+	if( !in ) { throw runtime_error( "when reading content to graph, istream invalid" ); }
 
-	for( unsigned i = 1; i <= vex_num; i++ ) {
+	for( unsigned i = 0; i < vex_num; i++ ) {
 		in >> vexs[i].first >> vexs[i].second;
 	}
 
 	T1 key_1, key_2;
 	T3 power;
-	for( unsigned i = 1; i <= arc_num; i++ ) {
+	for( unsigned i = 0; i < arc_num; i++ ) {
 		in >> key_1 >> key_2;
-		add_arc( key_1, key_2 );
+cout << "add arc: " << key_1 << "->" << key_2 << endl;
 		if( K == DiNetwork || K == UnDiNetwork ) {
 			in >> power;
 			add_arc( key_1, key_2, power );
@@ -172,6 +171,7 @@ istream &MGraph_basic< T1, T2, T3, K >::read_content( istream& in ) {
 			add_arc( key_2, key_1, power );
 		}
 
+		--arc_num; //Bcos the add_arc() will increase arc_num
 		int c;
 		while( (in >> c) && c != '\n' )
 			;
@@ -181,14 +181,14 @@ istream &MGraph_basic< T1, T2, T3, K >::read_content( istream& in ) {
 
 
 template< typename T1, typename T2, typename T3, GraphKind_t K >
-void MGraph_basic< T1, T2, T3, K >::add_vex( const T1& new_key, const T2& new_vex_value ) {
+void MGraph_basic< T1, T2, T3, K >::add_vex( const T1& new_key, const T2& new_vex_info ) {
 	++vex_num;
 	pair< T1, T2 > *new_vexs = new pair<T1, T2>[vex_num];
 	memcpy( new_vexs, vexs, sizeof( pair<T1, T2> ) * (vex_num-1) );
 	delete []vexs;
 	vexs = new_vexs;
 	vexs[vex_num-1].first = new_key;
-	vexs[vex_num-1].second = new_vex_value;
+	vexs[vex_num-1].second = new_vex_info;
 
 	T3 **new_arc_matrix = new T3*[vex_num];
 	memcpy( new_arc_matrix, arc_matrix, vex_num - 1 );
@@ -208,7 +208,7 @@ void MGraph_basic< T1, T2, T3, K >::add_vex( const T1& new_key, const T2& new_ve
 
 template< typename T1, typename T2, typename T3, GraphKind_t K >
 void MGraph_basic< T1, T2, T3, K >::remove_vex( const T1& key ) {
-	T2 value = get_value( key );
+	T2 info = get_info( key );
 	arc_num -= ( in_degree(key) + out_degree(key) );
 
 	unsigned loc = get_location( key );
@@ -316,14 +316,14 @@ unsigned MGraph_basic< T1, T2, T3, K >::out_degree( const T1& key ) {
 
 
 template< typename T1, typename T2, typename T3, GraphKind_t K >
-T2& MGraph_basic< T1, T2, T3, K >::get_value( const T1& key ) {
+T2& MGraph_basic< T1, T2, T3, K >::get_info( const T1& key ) {
 	for( unsigned i = 0; i < vex_num; i++ ) {
 		if ( vexs[i].first == key ) {
 			return vexs[i].second;
 		}
 	}
 
-	throw runtime_error( "try to get value of a non-existing key" );
+	throw runtime_error( "try to get info of a non-existing key" );
 }
 
 
@@ -340,9 +340,9 @@ unsigned MGraph_basic< T1, T2, T3, K >::get_location( const T1& key ) {
 
 
 template< typename T1, typename T2, typename T3, GraphKind_t K >
-void MGraph_basic< T1, T2, T3, K >::edit_value( const T1& key, const T2& new_value ) {
+void MGraph_basic< T1, T2, T3, K >::edit_info( const T1& key, const T2& new_info ) {
 	unsigned loc = get_location( key );
-	vexs[loc].second = new_value;
+	vexs[loc].second = new_info;
 }
 
 
